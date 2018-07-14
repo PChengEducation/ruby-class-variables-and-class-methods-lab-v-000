28817fb87b79a0070fab5f45224b402d4c324d78
Pan Cheng <pchengeducation@hotmail.com>
2018-07-14 04:26:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,19 @@
 class Song
 
+@@count = 0
+
 attr_accessor :name, :artist, :genre
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
+  @@artists << artist
   @genre = genre
+  @@genre << genre
+  @@count += 1
 end
 
-@@count
-@@artists = artists
-@@genres = genres
+
 
 
 
